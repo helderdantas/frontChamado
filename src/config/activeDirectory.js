53d08d0751b28c48508f9dfac95d1982c295f161
Deyves Oliveira <cristian_deyvinho@hotmail.com>
2023-06-02 10:02:48 -0300

    configuração do ad

---
+++ src/config/activeDirectory.js
@@ -0,0 +1,15 @@
+import { activedirectory } from activedirectory;
+import { dotenv } from dotenv;
+
+dotenv.config();
+
+const config = {
+    url: process.env.AD_URL,
+    baseDN: process.env.AD_BASE_DN,
+    username: process.env.AD_USERNAME,
+    password: process.env.AD_PASSWORD,
+};
+
+const ad = new activedirectory(config);
+
+export default ad;
